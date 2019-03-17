ceb335de2f6d8b29350a8ecb10e94d1a81653a4a
Sergejus <sergejus.kornejevas@stud.vgtu.lt>
2019-03-17 17:43:00 +0200

    0.1 versija

---
+++ duomenu-apdorojimas/GradeTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace duomenu_apdorojimas
+{
+    class GradeTable
+    {
+        const int tableWidth = 77;
+
+        public void printLine()
+        {
+            Console.WriteLine(new string('-', tableWidth));
+        }
+
+        public void printRow(string[] columns)
+        {
+            int width = (tableWidth - columns.Length) / columns.Length;
+            string row = "|";
+
+            foreach (string column in columns)
+            {
+                row += alignCentre(column, width) + "|";
+            }
+
+            Console.WriteLine(row);
+        }
+
+        protected string alignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }
+    }
+}
